daff8b7539aeaedde69cfdb20eb081b8d3d5d7f7
Tom Atterton <tpatterton@gmail.com>
2024-09-18 16:39:07 +0200

    Support image failure
    - add fix to aspect ration in third party package
    - fix 0m on horizontal cards
    - add stylesheet to image component

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from 'react';
 import Animated from 'react-native-reanimated';
 import { FasterImageView, FasterImageProps } from '@candlefinance/faster-image';
 import images from '@/theme/images';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 export type ImgProps = Omit<FasterImageProps, 'source'> & {
   className?: string;
@@ -22,26 +23,29 @@ const AnimatedImage = Animated.createAnimatedComponent(FasterImageView);
 
 const Image = ({ style, source, ...props }: ImgProps) => {
   const imageUri = useMemo(() => source?.url || source?.uri || '', [source]);
+  const { styles } = useStyles(stylesheet);
   return (
     <AnimatedImage
-      style={[
-        style,
-        {
-          overflow: 'hidden',
-        },
-      ]}
+      style={[style, styles.container]}
       source={{
-        resizeMode: 'cover',
         cachePolicy: 'discWithCacheControl',
         url: imageUri || images.PLACEHOLDER_IMAGE,
+        resizeMode: 'cover',
         showActivityIndicator: true,
         transitionDuration: 0.3,
         progressiveLoadingEnabled: true,
         ...source,
+        failureImage: images.PLACEHOLDER_IMAGE,
       }}
       {...props}
     />
   );
 };
+
+const stylesheet = createStyleSheet(() => ({
+  container: {
+    overflow: 'hidden',
+  },
+}));
 
 export default Image;
