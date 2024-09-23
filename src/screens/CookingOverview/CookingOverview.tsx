c748ad286d919ea100bd57037df24a3e925a85fe
Tom Atterton <tpatterton@gmail.com>
2024-09-23 16:25:53 +0200

    Fix large instructions

--- src/screens/CookingOverview/CookingOverview.tsx
+++ src/screens/CookingOverview/CookingOverview.tsx
@@ -1,5 +1,5 @@
-import React, { useMemo, useState } from 'react';
-import { View, FlatList, useWindowDimensions } from 'react-native';
+import React, { useCallback, useMemo, useState } from 'react';
+import { View, useWindowDimensions, ScrollView } from 'react-native';
 import useRecipeDetail from '@/hooks/recipe/useRecipeDetail';
 import { useRoute } from '@react-navigation/native';
 import type { RouteProp } from '@/navigation/types';
@@ -9,8 +9,9 @@ import MarkdownText from '@/screens/RecipeDetail/components/Instructions/Markdow
 import CloseButton from '@/components/CloseButton';
 import { Instruction } from '@/types';
 import { styleSheet } from '@/screens/CookingOverview/cookingOverview.style';
-import IngredientBottomSheet from '@/screens/CookingOverview/components/IngredientBottomSheet';
 import { flattenSanitizeIngredients } from '@/services/parser/ingredients/ingredientsParser';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import IngredientBottomSheet from '@/screens/CookingOverview/components/IngredientBottomSheet';
 
 const CookingOverview = () => {
   const {
@@ -40,23 +41,45 @@ const CookingOverview = () => {
     [ingredients],
   );
 
-  const renderInstruction = ({ item }: { item: Instruction }) => (
-    <View style={styles.instructionContainer}>
-      <MarkdownText
-        text={item?.text}
-        sanitizedIngredients={sanitizedIngredients}
-        ingredients={ingredients}
-        initialServings={initialServings}
-      />
-    </View>
+  const renderInstruction = useCallback(
+    ({ item }: { item: Instruction }) => (
+      <ScrollView contentContainerStyle={styles.instructionScrollContainer}>
+        <View style={styles.instructionContainer}>
+          <MarkdownText
+            text={item?.text}
+            sanitizedIngredients={sanitizedIngredients}
+            ingredients={ingredients}
+            initialServings={initialServings}
+          />
+        </View>
+      </ScrollView>
+    ),
+    [
+      styles.instructionScrollContainer,
+      styles.instructionContainer,
+      sanitizedIngredients,
+      ingredients,
+      initialServings,
+    ],
   );
+  const { height: screenHeight } = useWindowDimensions();
+
+  const MIN_SHEET_HEIGHT = screenHeight * 0.1;
+
+  const sheetHeight = useSharedValue(MIN_SHEET_HEIGHT);
+
+  const animatedContentContainerStyle = useAnimatedStyle(() => {
+    return {
+      paddingBottom: withTiming(sheetHeight.value, { duration: 300 }),
+    };
+  });
 
   return (
     <View style={styles.container}>
       <CloseButton />
-      <FlatList
+      <Animated.FlatList
         keyExtractor={(item) => item.id}
-        contentContainerStyle={styles.contentContainer}
+        style={animatedContentContainerStyle}
         data={instructions}
         horizontal
         pagingEnabled
@@ -65,6 +88,7 @@ const CookingOverview = () => {
         onScroll={handleScroll}
       />
       <IngredientBottomSheet
+        sheetHeight={sheetHeight}
         ingredients={ingredients}
         parsedIngredients={sanitizedIngredients}
         currentStep={currentIndex}
