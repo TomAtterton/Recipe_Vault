718f3ba25947cb344ecf013e42b2131368dc0e48
Tom Atterton <tpatterton@gmail.com>
2024-11-26 12:15:26 +0100

    Fix reminders

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   extends: ['@react-native-community', 'plugin:prettier/recommended', 'plugin:i18next/recommended'],
   plugins: [
@@ -7,22 +5,14 @@ module.exports = {
     'unused-imports',
     'react-native-unistyles',
     'eslint-plugin-react-compiler',
-    'jest', // Add jest plugin here
     'i18next',
   ],
-  env: {
-    'jest/globals': true, // Add jest environment here
-  },
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
+    project: './tsconfig.json',
   },
   rules: {
     'react-native-unistyles/no-unused-styles': 'warn',
     'react-compiler/react-compiler': 'warn',
-
-    // Define Prettier rules once
     'prettier/prettier': [
       'error',
       {
@@ -36,7 +26,8 @@ module.exports = {
         arrowParens: 'always',
       },
     ],
-    // Unused imports rule for all JavaScript/TypeScript files
+    'react/require-default-props': 'off', // Allow non-defined react props as undefined
+    '@typescript-eslint/comma-dangle': 'off', // Avoid conflict rule between ESLint and Prettier
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
       'error',
@@ -48,44 +39,10 @@ module.exports = {
     ],
   },
   overrides: [
-    // Specific rules for TypeScript files
     {
-      files: ['**/*.ts', '**/*.tsx'],
-      parserOptions: {
-        project: './tsconfig.json',
-      },
+      files: ['**/*.ts', '**/*.tsx', '**/*.js'],
       rules: {
-        '@typescript-eslint/comma-dangle': 'off', // Avoid conflict rule between Eslint and Prettier
-        'react/require-default-props': 'off', // Allow non-defined react props as undefined
-      },
-    },
-    // Specific rules for translations JSON files
-    {
-      files: ['src/translations/*.json'],
-      extends: ['plugin:i18n-json/recommended'],
-      rules: {
-        'i18n-json/valid-json': 'error',
-        'i18n-json/sorted-keys': [
-          'error',
-          {
-            order: 'asc',
-            indentSpaces: 2,
-          },
-        ],
-        'i18n-json/identical-keys': [
-          'error',
-          {
-            filePath: path.resolve('./src/translations/en.json'),
-          },
-        ],
-        // Override prettier for JSON formatting
-        'prettier/prettier': [
-          'error',
-          {
-            singleQuote: true,
-            endOfLine: 'auto',
-          },
-        ],
+        '@typescript-eslint/no-unused-vars': 'off', // Ensures no conflict with unused-imports
       },
     },
   ],
